5ab96981ea152665ee2061921523f418ddd3a7c7
Janus <janus@Fugl.(none)>
2012-02-13 22:20:01 +0100

    remove temp files

--- BotClient/bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/bin/classes.dex : \
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/libs/jackson-all-1.9.3.jar \
--- BotClient/bin/classes/R.java.d
+++
@@ -1,9 +0,0 @@
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/gen/de/tudarmstadt/botnet/janus_yanai/R.java \
- : /media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/res/drawable-hdpi/icon.png \
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/res/drawable-ldpi/icon.png \
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/res/drawable-mdpi/icon.png \
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/res/layout/camera.xml \
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/res/layout/main.xml \
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/res/menu/camera_menu.xml \
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/res/values/strings.xml \
-/media/sda1/Users/Janus/Documents/My Dropbox/TUD/android/BotClient/AndroidManifest.xml \
